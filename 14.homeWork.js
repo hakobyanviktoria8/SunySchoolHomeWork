a091eef6daff2ba8bebcff3d9ccd1e604dce76e5
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-05-17 22:11:31 +0400

    15.homeWork

--- 14.homeWork.js
+++ 14.homeWork.js
@@ -1,3 +1,56 @@
+// let form = document.getElementById("form");
+// // let fname = document.getElementById("fname");
+//
+// form.onsubmit = (e)=>{
+//     console.log("submited");
+//     // if (fname.value.length > 3){
+//     //     console.log(fname.value);
+//     // }
+//     // form.onsubmit();
+//     e.preventDefault();
+//     console.log(form.elements.fname.value);
+//     console.log(form.elements.lname.value);
+//     console.log(form.elements.mail.value);
+//     console.log(form.elements.number.value); //return first selected option in multiplay time
+//     // console.log([...select.selectedOptions]);
+// };
+//
+// window.addEventListener("paste",function () {
+//     console.log("paste")
+// });
+// window.addEventListener("copy",function () {
+//     console.log("copy")
+// });
+// form.elements.fname.addEventListener("focus",function () {
+//     console.log("focus")
+// });
+// form.elements.fname.addEventListener("blur",function () {
+//     console.log("blur")
+// });
+// form.elements.fname.addEventListener("change",function () {
+//     console.log("change")
+// });
+// form.elements.fname.addEventListener("input",function () {
+//     console.log("input",this.value)
+// });
+// // window.onload = (event) => {
+// //     console.log('The page has fully loaded');
+// // };
+// window.onunload = (event) => {
+//     console.log('The page has onunload');
+// };
+
+
+// window.onbeforeunload = function(event) {
+//     event.preventDefault()
+// };
+//
+// window.addEventListener('beforeunload', function (e) {
+//     e.preventDefault();
+//     e.returnValue = false;
+// });
+
+
 //1.Ունենք էջ որում կա input և a թեգ, որը հղում է դեպի sunnyschool.am: Եթե այցելուն սեղմում է հղման սեղմի ,
 // ծրագիրը մինչև հին էջը լքել ստուգի եթե այցելուն 10 վայրկյանից ավել է եղել էջում  ապա զգուշացնի որ փոփոխությունները
 // հնարավոր է չպահպանվեն:
@@ -33,12 +86,17 @@
 // 3.  Ունենք երկու input : Գրել ծրագիր որը թույլ չի տա երկրորդ input լրացնել
 // մինչև առաջին input-ում որևէ բան գրված չլինի:
 //
+
+
+//
+//
+//
 // window.addEventListener("load", function(){
 //     const fname = document.getElementById("fname");
 //     const lname = document.getElementById("lname");
 //
-//     lname.addEventListener("keydown", function (e) {
-//         fname.value.length === 0 && e.preventDefault();
+// //     lname.addEventListener("keydown", function (e) {
+// //         fname.value.length === 0 && e.preventDefault();
 //     });
 //     lname.addEventListener("focus", function (e) {
 //         fname.value.length === 0 && fname.focus();
@@ -49,6 +107,8 @@
 // սեղմելիս բոլորը կնշվեն երկրորդ անգամ նույն կոճակին սեղմենք բոլորը
 // կդառնան չնշված:
 //
+
+
 // window.addEventListener("load", function () {
 //     let inputs = document.querySelectorAll("#ex4 input");
 //     let button = document.getElementById("butt");
@@ -71,49 +131,87 @@
 // անհետանա հայտնվի Login Form, որտեղ եթե այցելուն ճիշտ հավաքի տվյալները
 // տեսնի տեքստ՝ welcome.
 
+const signUp = document.querySelector("#signUp form");
+const signIn = document.querySelector("#signIn form");
+let userData = {};
+signUp.onsubmit = (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-window.addEventListener("load" , function () {
-    const signUp = document.querySelector("#signUp form");
-    const signIn = document.querySelector("#signIn form");
-    let obj = null;
-
-    function validateEmail(email) {
-        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(email)) {
+        userData = {
+            email: email,
+        };
+    } else {
+        document.getElementById("email_error").innerText = "Error";
+        document.getElementById("email").style.border = "1px solid red"
     }
+    if (password.length < 7) {
+        userData = {
+            password: password
+        };
+    }
+    if (userData && email && password) {
+        console.log(userData, email, password);
+        document.getElementById("signUp").style.display = "none";
+        document.getElementById("signIn").style.display = "block"
+    }
+};
 
-    signUp.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
+signIn.onsubmit = (e) => {
+    e.preventDefault();
+    // console.log(userData);
+    if(emailSignIn.value ===  userData.email && passwordSignIn.value === userData.password){
+        document.getElementById("signIn").style.display ="none";
+        document.getElementById("welcome").style.display ="block";
+    }
+};
 
-        let bool = true;
-        if (!validateEmail(email)){
-            bool = false;
-        }
-        if (password < 7){
-            bool = false;
-        }
-        if (bool){
-            obj = {email, password};
-            document.getElementById("signUp").classList.add("hideElement");
-            document.getElementById("signIn").classList.remove("hideElement");
 
-            console.log(obj);
-        }
-    });
-
-    signIn.addEventListener("submit" , function (e) {
-        e.preventDefault();
-        const email = document.getElementById("emailSignIn").value;
-        const password = document.getElementById("passwordSignIn").value;
-
-        if (email === obj.email && password === obj.password){
-            document.getElementById("signIn").classList.add("hideElement");
-            document.getElementById("welcome").classList.remove("hideElement");
-        }
-    })
-});
+// window.addEventListener("load" , function () {
+//     const signUp = document.querySelector("#signUp form");
+//     const signIn = document.querySelector("#signIn form");
+//     let obj = null;
+//
+//     function validateEmail(email) {
+//         const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//         return re.test(email);
+//     }
+//
+//     signUp.addEventListener("submit", function (e) {
+//         e.preventDefault();
+//         const email = document.getElementById("email").value;
+//         const password = document.getElementById("password").value;
+//
+//         let bool = true;
+//         if (!validateEmail(email)){
+//             bool = false;
+//         }
+//         if (password < 7){
+//             bool = false;
+//         }
+//         if (bool){
+//             obj = {email, password};
+//             document.getElementById("signUp").classList.add("hideElement");
+//             document.getElementById("signIn").classList.remove("hideElement");
+//
+//             console.log(obj);
+//         }
+//     });
+//
+//     signIn.addEventListener("submit" , function (e) {
+//         e.preventDefault();
+//         const email = document.getElementById("emailSignIn").value;
+//         const password = document.getElementById("passwordSignIn").value;
+//
+//         if (email === obj.email && password === obj.password){
+//             document.getElementById("signIn").classList.add("hideElement");
+//             document.getElementById("welcome").classList.remove("hideElement");
+//         }
+//     })
+// });
 
 
 
--- 15.homeWork.js
+++ 15.homeWork.js
@@ -1,15 +1,172 @@
-//1.Ստեղծել օբեկտ որում կա երկու հատկություն՝ այցելուի էկրանի լայնությունը և բարձրությունը: Ստեղծել կոճակ որին սեղմելիս կիմանաք այդ արժեքները:
+// console.log(window.location);
+// console.log(window.location.href);
+// window.location.replace("./lolo");
+// window.location.reload();
+
+// console.log(window.history);
+// window.history.back();
+// window.history.forward();
+// window.history.go(-2);
+// console.log(window.navigator);
+// // console.log(window.navigator.userAgent);
+// window.navigator.geolocation.getCurrentPosition(function (data) {
+//     console.log(data);
+// });
+// setTimeout(() => {
+//     location.replace(' http://sunnyschool.am/ ');
+// }, 3000);
+
+// Example
+// function setCookie(cname, cvalue, exdays) {
+//     var d = new Date();
+//     d.setTime(d.getTime() + (exdays*24*60*60*1000));
+//     var expires = "expires="+ d.toUTCString();
+//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// }
+// setCookie("name","valod",1000);
 //
-// 2.Գրել ծրագիր , որը կստուգի եթե այցելու էկրանի չափը փոքր է 768px ապա հղումը փոխի դեպի m.facebook.com, այլապես facebook.com(location.replace())
 //
+//
+// function getCookie(cname) {
+//     var name = cname + "=";
+//     var decodedCookie = decodeURIComponent(document.cookie);
+//     var ca = decodedCookie.split(';');
+//     for(var i = 0; i <ca.length; i++) {
+//         var c = ca[i];
+//         while (c.charAt(0) == ' ') {
+//             c = c.substring(1);
+//         }
+//         if (c.indexOf(name) == 0) {
+//             return c.substring(name.length, c.length);
+//         }
+//     }
+//     return "";
+// }
+//
+// console.log(getCookie("name"));
+
+
+// let x = 10;
+// function timesTen(){
+//     let a = 2;
+//     return a * 10;
+// }
+// let y = timesTen(x);
+// console.log(y); // 100
+
+// function add(a, b) {
+//     return a + b;
+// }
+//
+// function average(a, b) {
+//     return add(a, b) / 2;
+// }
+//
+// let x = average(10, 20);
+//
+// console.log(x);
+
+//
+// function task(message) {
+//     // emulate time consuming task
+//     let n = 10000000000;
+//     while (n > 0){
+//         n--;
+//     }
+//     console.log(message);
+// }
+// // console.log('Start script...');
+// // task('Download a file.');
+// // console.log('Done!');
+//
+// console.log('Start script...');
+//
+// setTimeout(() => {
+//     task('Download a file.');
+// }, 1000);
+//
+// console.log('Done!');
+//
+
+
+//1.Ստեղծել օբեկտ որում կա երկու հատկություն՝ այցելուի էկրանի լայնությունը և բարձրությունը:
+// Ստեղծել կոճակ որին սեղմելիս կիմանաք այդ արժեքները:
+//
+//
+// click1.onclick = () => {
+//     console.log("Height = ",window.screen.height,"width = ",window.screen.width);
+// };
+
+// 2.Գրել ծրագիր , որը կստուգի եթե այցելու էկրանի չափը փոքր է 768px ապա հղումը փոխի դեպի
+// m.facebook.com, այլապես facebook.com(location.replace())
+//
+//
+// window.location.replace(window.screen.width > 768 ? "https://facebook.com" : "https://m.facebook.com");
+// console.log(window.location);
+
 // 3.Ունենք form , որով այցելուն մուտքագրում է իր էլ. հասցեն  և submit անում:
-// Ստեղծել ծրագիր , որը չերևացող input որպես value կգրի YES եթե այցելու բրաուզերը  Chrome (navigator.userAgent.indexOf("Chrome")) է և NO եթե այլ բրաուզեր է : Submit ժամանակ այդ արժեքը նույնպես պետք է ուղարկվի սերվեր:
+// Ստեղծել ծրագիր , որը չերևացող input որպես value կգրի YES եթե այցելու բրաուզերը
+// Chrome (navigator.userAgent.indexOf("Chrome")) է և NO եթե այլ բրաուզեր է : Submit ժամանակ
+// այդ արժեքը նույնպես պետք է ուղարկվի սերվեր:
 // Submit ժամանակ արտածել input value-ները (, իսկ event.preventDefault() Submit ժամանակ գործողությունը կանգնեցնելու համար):
+// //
+// document.getElementById("form").addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     this.elements.text1.value = window.navigator.userAgent.includes("Chrome") ? "YES" : "NO";
+//     console.log("here");
+//     this.submit();
+// });
+
+
+// 4.Ունենք հղում դեպի sunnyschool.am:Գրել ծրագիր որ այցելուն երբ սեղմի հղման
+// լրացուցիչ հարց ստանա արդյոք հղում բացի նոր window թե նույնի  մեջ:
+// Եթե այցելուն confirm անի թող բացի նոր window մեջ, եթե ոչ ապա նույն էջում բացի
+// Sunny School կայքը:
 //
-// 4.Ունենք հղում դեպի sunnyschool.am:Գրել ծրագիր որ այցելուն երբ սեղմի հղման լրացուցիչ հարց ստանա արդյոք հղում բացի նոր window թե նույնի  մեջ:
-// Եթե այցելուն confirm անի թող բացի նոր window մեջ, եթե ոչ ապա նույն էջում բացի Sunny School կայքը:
 //
-// 5.Ունենք HTML,CSS,Bootstrap,JavaScript, jQuery, Node.JS զանգված: Գրել ծրագիր որով այցելու կարող է թերթել այս ցանկի բոլոր անդամներ երբ սեղմի առաջ կամ հետ գնալու կոճակներ,
-// կամ ծրագիրը ավտոմատ 3 վարկյան մեկ փոխի:Էջը բացելիս պետք է երևա զանգված առաջին անդամը, երբ առաջ գնալով հասնենք վերջին անդամին (Node.JS) դրանից հետո առաջ սեղմելիս երևա կրկին առաջին անդամը,
+// document.getElementById("aa").addEventListener("click", function (e) {
+//     e.preventDefault();
+//     let bool = confirm("Yes or No");
+//     bool ? window.open(this.getAttribute("href")): window.location.href = this.getAttribute("href");
+// });
+
+
+// 5.Ունենք HTML,CSS,Bootstrap,JavaScript, jQuery, Node.JS զանգված:
+// Գրել ծրագիր որով այցելու կարող է թերթել այս ցանկի բոլոր անդամներ երբ սեղմի առաջ կամ հետ գնալու կոճակներ,
+// կամ ծրագիրը ավտոմատ 3 վարկյան մեկ փոխի: Էջը բացելիս պետք է երևա զանգված առաջին անդամը,
+// երբ առաջ գնալով հասնենք վերջին անդամին (Node.JS) դրանից հետո առաջ սեղմելիս երևա կրկին առաջին անդամը,
 // իսկ երբ առաջին անդամից հետ ուզենք գնանք երևա զանգվածի վերջին անդամը (Նկ. Home5.jpg):
+
+let arr = ["HTML", "CSS", "Bootstrap", "JavaScript", "jQuery", "Node.JS"];
+let span = document.getElementById("span");
+
+let i = 0;
+span.innerText = arr[i];
+
+const right = function () {
+    i++;
+    if (i > arr.length - 1) {
+        i = 0;
+    }
+    span.innerText = arr[i];
+};
+document.getElementById("Next").onclick = right;
+document.getElementById("Prev").onclick = () => {
+    if (i === 0) {
+        i = arr.length - 1
+    } else {
+        i--;
+    }
+    span.innerText = arr[i];
+};
+
+setInterval(right, 3000);
+
+
+
+
+
+
+
+
 
